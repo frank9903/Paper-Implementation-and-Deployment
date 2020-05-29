fa07615882d3c87503b0d51f18c1b7b8d9241912
S53CAO <s53cao@uwaterloo.ca>
2020-05-28 20:08:41 -0400

    enable auto completion for user input

--- Deployment/Deployment/View Controllers/Image Caption /Image Caption Controller.swift
+++ Deployment/Deployment/View Controllers/Image Caption /Image Caption Controller.swift
@@ -27,19 +27,27 @@ class ImageCaptionController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.input.contentMode = .scaleAspectFit
+        self.imagePicker = ImagePicker(presentationController: self, delegate: self)
+
+        self.result.delegate = self
         self.result.font = UIFont(name: "American Typewriter", size: 24)
         self.result.textColor = .white
-        self.imagePicker = ImagePicker(presentationController: self, delegate: self)
+        self.result.isUserInteractionEnabled = false // disable user interaction if no image present
+
         // load dictionary
         let dict:[String:String] = json2dict(name: "idx2word")!
         for (idxStr, wordStr) in dict {
             idx2word[Int(idxStr)!] = wordStr
             word2idx[wordStr] = Int(idxStr)!
         }
+        // keyboard management: tap to dismiss
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+        view.addGestureRecognizer(tap)
     }
     
     @IBAction func pickImage(_ sender: Any) {
         self.imagePicker.present(from: sender as! UIView)
+        self.result.isUserInteractionEnabled = true // enable user correction for caption
     }
 
     func json2dict(name:String) -> [String:String]? {
@@ -65,45 +73,55 @@ class ImageCaptionController: UIViewController {
             print("map failure")
             return
         }
-        let initStates = mapResults.featureValue(for: "init_states")?.multiArrayValue
         
-
-        var maxVal = 0.0, maxInd = word2idx["<start>"]
+        let initStates = mapResults.featureValue(for: "init_states")!.multiArrayValue!
         var hiddenStates = Array(repeating: initStates, count: 3)
-        var sentence = ""
+        let prefix = result.text!.lowercased() // user input prefix
+        var sentence = prefix
+        let wordsList = ["<start>"] + prefix.words
+        
+        var curIdx = -1
+        for word in wordsList {
+            let idx = word2idx[word] ?? word2idx["<end>"]!
+            curIdx = forward1step(input: idx, hiddenStates: &hiddenStates)
+        }
         
         for _ in 1...MAX_LEN {
-            let mlArray = try? MLMultiArray(shape: [1], dataType: MLMultiArrayDataType.int32)
-            mlArray?[0] = NSNumber(value: maxInd!)
-            
-            guard let results = try? GRU.prediction(decoder_input: mlArray!, decoder_gru1_h_in: hiddenStates[0], decoder_gru2_h_in: hiddenStates[1], decoder_gru3_h_in: hiddenStates[2]) else {
-                print("decoder failure")
-                return
-            }
-            
-            hiddenStates[0] = results.featureValue(for: "decoder_gru1_h_out")?.multiArrayValue!
-            hiddenStates[1] = results.featureValue(for: "decoder_gru2_h_out")?.multiArrayValue!
-            hiddenStates[2] = results.featureValue(for: "decoder_gru3_h_out")?.multiArrayValue!
-            
-            let one_hot = results.featureValue(for: "decoder_output")?.multiArrayValue!
-            let length = one_hot!.count
-            maxVal = 0.0
-            for i in 0..<length {
-                let x = Double(truncating: one_hot![i])
-                if x > maxVal {
-                    maxVal = x
-                    maxInd = i
-                }
-            }
-            
-            if maxInd == word2idx["<end>"] {
+            if curIdx == word2idx["<end>"] {
                 break
             }
-            sentence += idx2word[maxInd!]! + " "
-            
+            sentence += " " + idx2word[curIdx]!
+            curIdx = forward1step(input: curIdx, hiddenStates: &hiddenStates)
         }
         result.text = sentence
         print("Result sentence: \(sentence)")
+    }
+    
+    func forward1step(input:Int, hiddenStates:inout [MLMultiArray]) -> Int {
+        let decoderInput = try? MLMultiArray(shape: [1], dataType: MLMultiArrayDataType.int32)
+        decoderInput?[0] = NSNumber(value: input)
+        
+        guard let results = try? GRU.prediction(decoder_input: decoderInput!, decoder_gru1_h_in: hiddenStates[0], decoder_gru2_h_in: hiddenStates[1], decoder_gru3_h_in: hiddenStates[2]) else {
+            print("decoder failure")
+            return -1
+        }
+        
+        hiddenStates[0] = results.featureValue(for: "decoder_gru1_h_out")!.multiArrayValue!
+        hiddenStates[1] = results.featureValue(for: "decoder_gru2_h_out")!.multiArrayValue!
+        hiddenStates[2] = results.featureValue(for: "decoder_gru3_h_out")!.multiArrayValue!
+        let one_hot = results.featureValue(for: "decoder_output")!.multiArrayValue!
+        let length = one_hot.count
+        
+        var maxVal = 0.0, maxInd = -1
+        for i in 0..<length {
+            let x = Double(truncating: one_hot[i])
+            if x > maxVal {
+                maxVal = x
+                maxInd = i
+            }
+        }
+        
+        return maxInd
     }
     
     func generateCaption() {
@@ -112,6 +130,10 @@ class ImageCaptionController: UIViewController {
         try! handler.perform([request])
     }
     
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+        generateCaption()
+    }
 }
 
 // image picker
@@ -120,7 +142,22 @@ extension ImageCaptionController: ImagePickerDelegate {
     func didSelect(image: UIImage?) {
         if image != nil {
             self.input.image = image
+            self.result.text = "" // clear last results
             generateCaption()
         }
+    }
+}
+
+// keyboard management: return to dismiss
+extension ImageCaptionController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.dismissKeyboard()
+        return false
+    }
+}
+
+extension StringProtocol {
+    var words: [String] {
+        return split{ !$0.isLetter }.map { String($0) }
     }
 }
